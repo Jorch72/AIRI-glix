26ec3d9d8d183b4b7c61143211a2fadc037467c0
Ri5ux <dustinchristensen@outlook.com>
2014-11-24 19:55:25 -0600

    Added ModelTexMap to map a ModelBase to a ResourceLocation without using hashmaps.

---
+++ src/main/java/com/arisux/airi/lib/util/ModelTexMap.java
@@ -0,0 +1,16 @@
+package com.arisux.airi.lib.util;
+
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.util.ResourceLocation;
+
+public class ModelTexMap
+{
+	public ResourceLocation resourceLocation;
+	public ModelBase modelBase;
+	
+	public ModelTexMap(ModelBase modelBase, ResourceLocation resourceLocation)
+	{
+		this.modelBase = modelBase;
+		this.resourceLocation = resourceLocation;
+	}
+}
