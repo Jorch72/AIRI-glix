873ed336cf6565f09223ae91dd79fcc79be806c4
Ri5ux <dustinchristensen@outlook.com>
2014-12-03 08:19:58 -0600

    Fixed a problem loading mcmod.info outside of the dev environment.

--- src/main/java/com/arisux/airi/lib/util/interfaces/ModController.java
+++ src/main/java/com/arisux/airi/lib/util/interfaces/ModController.java
@@ -15,46 +15,61 @@ public abstract class ModController implements IInitializablePre, IInitializable
 	private String modId, changelogUrl, beta;
 	private ModContainer modContainer;
 	private JsonElement modInfoJson;
-	
+
 	public abstract CreativeTabs getCreativeTab();
-	
+
 	public Object mod()
 	{
 		return this.container().getMod();
 	}
-	
+
 	public ModContainer container()
 	{
 		return this.modContainer == null ? this.modContainer = ModEngine.getModContainerForId(this.id()) : this.modContainer;
 	}
-	
+
 	public String id()
 	{
 		return this.modId == null ? this.modId = ModEngine.getAnnotatedModId(this.getClass()) : this.modId;
 	}
-	
+
 	public String domain()
 	{
 		return this.modId + ":";
 	}
-	
+
 	public String channel()
 	{
 		return "CHANNEL_" + this.id().toUpperCase();
 	}
-	
+
 	public String changelogUrl()
 	{
 		return this.changelogUrl == null ? this.getModInfoJson() != null ? this.changelogUrl = JsonUtils.getJsonElementAsJsonObject(this.getModInfoJson(), "changelogUrl").get("changelogUrl").getAsString() : "" : this.changelogUrl;
 	}
-	
+
 	public boolean isBetaRelease()
 	{
 		return Boolean.parseBoolean(this.beta == null ? this.getModInfoJson() != null ? this.beta = String.valueOf(JsonUtils.getJsonElementAsJsonObject(this.getModInfoJson(), "beta").get("beta").getAsBoolean()) : "true" : this.beta);
 	}
-	
+
 	public JsonElement getModInfoJson()
 	{
-		return this.modInfoJson == null ? this.modInfoJson = ModEngine.parseJsonFromFile(new File(getClass().getResource("/mcmod.info").getFile())) : this.modInfoJson;
+		if (this.modInfoJson == null)
+		{
+			String filename = "/mcmod.info";
+			File jsonFile = null;
+			
+			if (getClass().getResource(filename) != null)
+			{
+				jsonFile = new File(getClass().getResource(filename).getFile());
+			}
+			else
+			{
+				jsonFile = new File(getClass().getClassLoader().getResource(filename).getFile());
+			}
+			return this.modInfoJson = ModEngine.parseJsonFromFile(jsonFile);
+		}
+		return this.modInfoJson;
 	}
 }
