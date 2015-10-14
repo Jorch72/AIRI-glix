eb2f1df279733e828f585c1822a6bcef09cbe4be
Dustin Christensen <Ri5ux@users.noreply.github.com>
2015-10-14 01:39:53 -0500

    Updated AIRI's build information.

--- src/main/java/com/arisux/airi/Properties.java
+++ src/main/java/com/arisux/airi/Properties.java
@@ -4,6 +4,5 @@ public class Properties
 {
 	public static final String MODID = "AIRI";
 	public static final String CHANNEL = "CHANNEL_" + MODID;
-	public static final String VERSION = "${version}";
 	public final String DOMAIN = "airi:";
 }
