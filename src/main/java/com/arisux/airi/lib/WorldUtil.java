a154790834b3d0c3e28c5e81124cd368b28e55ed
Dustin Christensen <Ri5ux@users.noreply.github.com>
2016-03-16 16:45:03 -0500

    Fix for NullPointerExceptions caused by entities on world startup.

--- src/main/java/com/arisux/airi/lib/WorldUtil.java
+++ src/main/java/com/arisux/airi/lib/WorldUtil.java
@@ -1341,7 +1341,12 @@ public class WorldUtil
 		
 	    public static boolean isInMaterial(Entity entity, Material material)
 	    {
-	        return entity.worldObj.isMaterialInBB(entity.getBoundingBox().expand(-0.10000000149011612D, -0.4000000059604645D, -0.10000000149011612D), material);
+	    	if (entity != null && entity.worldObj != null)
+	    	{
+	    		return entity.worldObj.isMaterialInBB(entity.getBoundingBox().expand(-0.10000000149011612D, -0.4000000059604645D, -0.10000000149011612D), material);
+	    	}
+	    	
+	    	return false;
 	    }
 
 		public static class Players
