2147e7ff751682cc887ab229c61a652bb9cba6e5
Ri5ux <Ri5ux@users.noreply.github.com>
2016-05-21 16:27:26 -0500

    Added Texture class. Makes it much easier to bind textures.

---
+++ src/main/java/com/arisux/airi/lib/client/Texture.java
@@ -0,0 +1,23 @@
+package com.arisux.airi.lib.client;
+
+import com.arisux.airi.lib.RenderUtil;
+
+import net.minecraft.util.ResourceLocation;
+
+public class Texture extends ResourceLocation
+{
+    public Texture(String location)
+    {
+        super(location);
+    }
+    
+    public Texture(String domain, String location)
+    {
+        super(domain, location);
+    }
+    
+    public void bindTexture()
+    {
+        RenderUtil.bindTexture(this);
+    }
+}
