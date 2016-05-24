8f6494c83adaca2ad410a4c20a4075b0a387624d
Hassan Syyid <hassansyyid@gmail.com>
2016-05-23 21:35:11 -0400

    Fix Texture Class Being Client-Side Only

--- src/main/java/com/arisux/airi/lib/client/Texture.java
+++ src/main/java/com/arisux/airi/lib/client/Texture.java
@@ -6,26 +6,26 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.util.ResourceLocation;
 
-@SideOnly(Side.CLIENT)
 public class Texture extends ResourceLocation
 {
-    public Texture(ResourceLocation resource)
-    {
-        this(resource.getResourceDomain(), resource.getResourcePath());
-    }
-    
-    public Texture(String location)
-    {
-        super(location);
-    }
-    
-    public Texture(String domain, String location)
-    {
-        super(domain, location);
-    }
-    
-    public void bind()
-    {
-        RenderUtil.bindTexture(this);
-    }
+	public Texture(ResourceLocation resource)
+	{
+		this(resource.getResourceDomain(), resource.getResourcePath());
+	}
+
+	public Texture(String location)
+	{
+		super(location);
+	}
+
+	public Texture(String domain, String location)
+	{
+		super(domain, location);
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void bind()
+	{
+		RenderUtil.bindTexture(this);
+	}
 }
