0f1a89152d14a034f17b273b9670956f01156a68
Ri5ux <Ri5ux@users.noreply.github.com>
2016-05-23 21:55:44 -0500

    Fixed the formatting.

--- src/main/java/com/arisux/airi/lib/client/Texture.java
+++ src/main/java/com/arisux/airi/lib/client/Texture.java
@@ -8,24 +8,24 @@ import net.minecraft.util.ResourceLocation;
 
 public class Texture extends ResourceLocation
 {
-	public Texture(ResourceLocation resource)
-	{
-		this(resource.getResourceDomain(), resource.getResourcePath());
-	}
+    public Texture(ResourceLocation resource)
+    {
+        this(resource.getResourceDomain(), resource.getResourcePath());
+    }
 
-	public Texture(String location)
-	{
-		super(location);
-	}
+    public Texture(String location)
+    {
+        super(location);
+    }
 
-	public Texture(String domain, String location)
-	{
-		super(domain, location);
-	}
+    public Texture(String domain, String location)
+    {
+        super(domain, location);
+    }
 
-	@SideOnly(Side.CLIENT)
-	public void bind()
-	{
-		RenderUtil.bindTexture(this);
-	}
+    @SideOnly(Side.CLIENT)
+    public void bind()
+    {
+        RenderUtil.bindTexture(this);
+    }
 }
